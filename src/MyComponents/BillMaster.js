3b0eef825c7efc24b881d2107774e2cd35af391d
JackieJain <155225240+JackieJain@users.noreply.github.com>
2025-02-14 19:12:05 +0530

    Update BillMaster.js

--- src/MyComponents/BillMaster.js
+++ src/MyComponents/BillMaster.js
@@ -1,43 +1,56 @@
-import React from 'react'
+import React from "react";
 
 export default function BillMaster(props) {
-
-    const deleteBill = (index) => {
-        
-    }
+  const deleteBill = (Id) => {
+    props.setbills(props.getbills.filter((x) => x.Id != Id));
+  };
 
   return (
-    <div className="container" style={{marginTop: '50px'}}>
-      <table className="table" border="1">
-        <thead>
+    <div className="card container" style={{ marginTop: "50px" }}>
+      <div className="card-header">
+        <h3>Bill Master</h3>
+      </div>
+      <div className="card-body">
+        <table className="table table-bordered mt-4" border="1">
+          <thead>
             <tr>
-                <th scope="col">#</th>
-                <th scope="col">Bill Name</th>
-                <th scope="col">Bill Amount</th>
-                <th scope="col">Bill Date</th>
-                <th scope="col">Bill Upload</th>
-                <th scope="col">Action</th>
+              <th scope="col">Sr No.</th>
+              <th scope="col">Bill Name</th>
+              <th scope="col">Bill Amount</th>
+              <th scope="col">Bill Date</th>
+              <th scope="col">Bill Upload</th>
+              <th scope="col">Action</th>
             </tr>
-            </thead>
+          </thead>
 
-            { props.bills.map((bill, index) => {    
-                return (
-                    <tbody key={index}>
-                        <tr>
-                            <th scope="row">{index + 1}</th>
-                            <td>{bill.billName}</td>
-                            <td>{bill.billAmount}</td>
-                            <td>{bill.billDate}</td>
-                            <td>{bill.billupload}</td>
-                            <td>
-                                <button className="btn btn-primary" >Edit</button>
-                                <button className="btn btn-danger">Delete</button>
-                            </td>
-                        </tr>
-                    </tbody>
-                )  }) 
-            }
+          {props.getbills.map((bill, index) => {
+            return (
+              <tbody key={index}>
+                <tr>
+                  <td id="rowId">{bill.Id}</td>
+                  <td>{bill.billName}</td>
+                  <td>{bill.billAmount}</td>
+                  <td>{bill.billDate}</td>
+                  <td>
+                    <img src={bill.billupload}></img>{" "}
+                  </td>
+                  <td>
+                    <button className="btn btn-primary ml-2">Edit</button>
+                    <button
+                      className="btn btn-danger"
+                      onClick={() => {
+                        deleteBill(bill.Id);
+                      }}
+                    >
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              </tbody>
+            );
+          })}
         </table>
+      </div>
     </div>
-  )
+  );
 }
